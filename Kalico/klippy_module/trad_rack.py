8741241072dca78925e428d39aea2a28c37823f5
mk-maddin <46523240+mk-maddin@users.noreply.github.com>
2025-08-06 06:54:36 +0200

    fix copy&paste input

--- Kalico/klippy_module/trad_rack.py
+++ Kalico/klippy_module/trad_rack.py
@@ -979,7 +979,7 @@ class TradRack:
         self.sync_to_extruder = True
         self._restore_extruder_sync()
 
-    int(gcmd.get_command().partition("T")[2])TR_UNSYNC_FROM_EXTRUDER_help = (
+    TR_UNSYNC_FROM_EXTRUDER_help = (
         "Unsync Trad Rack's filament driver from the extruder"
     )
 
